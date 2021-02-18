f51d3469f3424ba48b85675303f94b6be8214af8
aru-nur <nurakhmet.aruzhan@gmail.com>
2021-02-18 23:51:46 +0600

    altinwi commit

--- app/src/main/java/kz/course/calculator2/MainActivity.java
+++ app/src/main/java/kz/course/calculator2/MainActivity.java
@@ -34,113 +34,141 @@ public class MainActivity extends AppCompatActivity {
         buttonm = findViewById(R.id.btnm);
         buttonn = findViewById(R.id.btnn);
         tvSan = findViewById(R.id.tv_san);
-        buttonDel .setOnClickListener(new View.OnClickListener() {
+
+        View.OnClickListener buttonSandar = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tvSan.setText("Del");
+                switch (View.getId()) {
+                    case R.id.btnDel
+                        ;
+                        tvSan.setText("Del");
+                        break;
+                    case R.id.btny
+                        ;
+                        tvSan.setText("=");
+                        break;
+                    case R.id.btn1:
+                        String aldingiSan = tvSan.getText().toString();
+                        if (aldingiSan.equals("0")) {
+                            tvSan.setText("1");
+                        } else {
+                            tvSan.setText(aldingiSan + "1");
+                        }
+                        break;
+
+                    case R.id.btn2:
+                    String aldingiSan = tvSan.getText().toString();
+                        if (aldingiSan.equals("0")) {
+                            tvSan.setText("2");
+                        } else {
+                        tvSan.setText(aldingiSan+"2");
+                        break;
+                    case R.id.btn3;
+                        String aldingiSan = tvSan.getText().toString();
+                        if (aldingiSan.equals("0")) {
+                            tvSan.setText("3");
+                        } else {
+                        tvSan.setText(aldingiSan+"3");
+                        tvSan.setText("3");
+                        break;
+                        case R.id.btnj:
+                        tvSan.setText("/");
+                        break;
+                            case R.id.btn4:
+                        String aldingiSan = tvSan.getText().toString();
+                                if (aldingiSan.equals("0")) {
+                                    tvSan.setText("4");
+                                } else {
+                        tvSan.setText(aldingiSan+"4");
+                        tvSan.setText("4");
+                        break;
+                                    case R.id.btn5:
+                        String aldingiSan = tvSan.getText().toString();
+                                        if (aldingiSan.equals("0")) {
+                                            tvSan.setText("5");
+                                        } else {
+                        tvSan.setText(aldingiSan+"5");
+                        tvSan.setText("5");
+                        break;
+                                            case R.id.btn6:
+                        String aldingiSan = tvSan.getText().toString();
+                        if (aldingiSan.equals("0")) {
+                            tvSan.setText("6");
+                        } else {
+                        tvSan.setText(aldingiSan+"6");
+                        tvSan.setText("6");
+                        break;
+                            case R.id.btnk:
+                        tvSan.setText("-");
+                        break;
+                            case R.id.btn7:
+                        String aldingiSan = tvSan.getText().toString();
+                                if (aldingiSan.equals("0")) {
+                                    tvSan.setText("7");
+                                } else {
+                        tvSan.setText(aldingiSan+"7");
+                        tvSan.setText("7");
+                        break;
+                                    case R.id.btn8:
+                        String aldingiSan = tvSan.getText().toString();
+                                        if (aldingiSan.equals("0")) {
+                                            tvSan.setText("8");
+                                        } else {
+                        tvSan.setText(aldingiSan+"8");
+                        tvSan.setText("8");
+                        break;
+                                            case R.id.btn9:
+                        String aldingiSan = tvSan.getText().toString();
+                                                if (aldingiSan.equals("0")) {
+                                                    tvSan.setText("9");
+                                                } else {
+                        tvSan.setText(aldingiSan+"9");
+                        tvSan.setText("9");
+                        break;
+                                                    case R.id.btnl:
+                        tvSan.setText("*");
+                        break;
+                                                    case R.id.btne:
+                        tvSan.setText(".");
+                        break;
+                                                    case R.id.btnt:
+                        String aldingiSan = tvSan.getText().toString();
+                                                        if (aldingiSan.equals("0")) {
+                                                            tvSan.setText("0");
+                                                        } else {
+                        tvSan.setText(aldingiSan+"0");
+                        tvSan.setText("0");
+                        break;
+                                                            case R.id.btnm:
+                        tvSan.setText("%");
+                        break;
+                                                            case R.id.btnn:
+                        tvSan.setText("+");
+                        break;
+
+
+                }
+
             }
-        });
-        buttony .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("=");
-            }
-        });
-        button1 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("1");
-            }
-        });
-        button2 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("2");
-            }
-        });
-        button3 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("3");
-            }
-        });
-        buttonj .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("/");
-            }
-        });
-        button4 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("4");
-            }
-        });
-        button5 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("5");
-            }
-        });
-        button6 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("6");
-            }
-        });
-        buttonk .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("-");
-            }
-        });
-        button7 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("7");
-            }
-        });
-        button8 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("8");
-            }
-        });
-        button9 .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("9");
-            }
-        });
-        buttonl .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("*");
-            }
-        });
-        buttone .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText(".");
-            }
-        });
-        buttont .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("0");
-            }
-        });
-        buttonm .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("%");
-            }
-        });
-        buttonn .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvSan.setText("+");
-            }
-        });
+        };
+
+        buttonDel .setOnClickListener(buttonSandar);
+        buttony .setOnClickListener(buttonSandar);
+        button1 .setOnClickListener(buttonSandar);
+        button2 .setOnClickListener(buttonSandar);
+        button3 .setOnClickListener(buttonSandar);
+        buttonj .setOnClickListener(buttonSandar);
+        button4 .setOnClickListener(buttonSandar);
+        button5 .setOnClickListener(buttonSandar);
+        button6 .setOnClickListener(buttonSandar);
+        buttonk .setOnClickListener(buttonSandar);
+        button7 .setOnClickListener(buttonSandar);
+        button8 .setOnClickListener(buttonSandar);
+        button9 .setOnClickListener(buttonSandar);
+        buttonl .setOnClickListener(buttonSandar);
+        buttone .setOnClickListener(buttonSandar);
+        buttont .setOnClickListener(buttonSandar);
+        buttonm .setOnClickListener(buttonSandar);
+        buttonn .setOnClickListener(buttonSandar);
     }
 }
